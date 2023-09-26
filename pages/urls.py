e7de65f9937ef1abde21f56f69b52afe4f140102
unknown <NiloofarSeyf>
2023-09-26 23:56:03 +0330

    create custome user with forms and signup

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from .views import HomePageView
+
+urlpatterns = [
+    path('home/', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
+
+]
