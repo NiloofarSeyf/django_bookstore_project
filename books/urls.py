7a90236e07e6472babd41e874b5a54ae2b36d385
unknown <NiloofarSeyf>
2023-10-11 11:53:06 +0330

    add media config to project

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+
 from .views import BookListView, BookDetailView, BookCreateView,BookUpdateView, BookDeleteView
 
 
--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # static files config
 STATIC_URL = '/statics/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
