224c2f137f0f893fb04cd40eaeaf2f95fbb11ccd
unknown <NiloofarSeyf>
2023-10-13 09:45:26 +0330

    add comment form to book detail view

--- books/models.py
+++ books/models.py
@@ -27,5 +27,8 @@ class Comment(models.Model):
 
     datetime_created = models.DateTimeField(auto_now_add=True)
 
+    is_active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
+
     def __str__(self):
         return self.text
